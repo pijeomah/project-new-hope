3a31b7ade15eac5dec7403420f4d7866a31da832
Onyedika <promiseij4919@gmail.com>
2024-12-31 22:07:41 +0000

    finished the contact section and the light and dark mode

--- app.js
+++ app.js
@@ -34,5 +34,13 @@ function pageTransitions(){
            element.classList.add('active')
        }
 })
+
+
+//toggle theme
+const themeBtn = document.querySelector('.theme-btn');
+themeBtn.addEventListener('click', () => {
+    let element = document.body
+    element.classList.toggle('light-mode')
+})
 }
 pageTransitions();
